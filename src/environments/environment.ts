29db0277a95a9eff22d888a5a642844a47a62327
andrisole92 <andrisole92@interstruct.com>
2018-10-26 14:45:26 -0400

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export
+const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCv5vFKOY9-jVxGGtxWJi6glBF6WyQpFpo",
+    authDomain: "fairytrail-33178.firebaseapp.com",
+    databaseURL: "https://fairytrail-33178.firebaseio.com",
+
+    projectId: "fairytrail-33178",
+    storageBucket: "fairytrail-33178.appspot.com",
+    messagingSenderId: "97338720144"
+  }
+};
